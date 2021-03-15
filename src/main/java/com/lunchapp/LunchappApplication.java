be857d70aca50cd15a2b00baeafbad8bb47b07d3
mbc <audqja3401@gmail.com>
2021-03-15 22:57:06 +0900

    주석 추가

--- src/main/java/com/lunchapp/LunchappApplication.java
+++ src/main/java/com/lunchapp/LunchappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LunchappApplication {
 
 	public static void main(String[] args) {
+		// 주석 처리
 		SpringApplication.run(LunchappApplication.class, args);
 	}
 
